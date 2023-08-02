e7922d0d47496c8faab1790b74932d644556a39c
Pfunzop <111533324+Pfunzop@users.noreply.github.com>
2023-08-02 21:00:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace mockexammmm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtnumber = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtcon = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Type = new System.Windows.Forms.ListBox();
+            this.btnprocess = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "lucky number";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Confirm Password";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(108, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtnumber
+            // 
+            this.txtnumber.Location = new System.Drawing.Point(108, 66);
+            this.txtnumber.Name = "txtnumber";
+            this.txtnumber.Size = new System.Drawing.Size(100, 20);
+            this.txtnumber.TabIndex = 5;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(136, 29);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(100, 20);
+            this.txtpass.TabIndex = 6;
+            // 
+            // txtcon
+            // 
+            this.txtcon.Location = new System.Drawing.Point(136, 66);
+            this.txtcon.Name = "txtcon";
+            this.txtcon.Size = new System.Drawing.Size(100, 20);
+            this.txtcon.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Type of user";
+            // 
+            // Type
+            // 
+            this.Type.FormattingEnabled = true;
+            this.Type.Location = new System.Drawing.Point(108, 92);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(120, 69);
+            this.Type.TabIndex = 9;
+            // 
+            // btnprocess
+            // 
+            this.btnprocess.Location = new System.Drawing.Point(182, 269);
+            this.btnprocess.Name = "btnprocess";
+            this.btnprocess.Size = new System.Drawing.Size(75, 23);
+            this.btnprocess.TabIndex = 10;
+            this.btnprocess.Text = "Process";
+            this.btnprocess.UseVisualStyleBackColor = true;
+            this.btnprocess.Click += new System.EventHandler(this.btnprocess_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(309, 269);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 11;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(424, 269);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(75, 23);
+            this.btnexit.TabIndex = 12;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // Display
+            // 
+            this.Display.FormattingEnabled = true;
+            this.Display.Location = new System.Drawing.Point(134, 322);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(471, 95);
+            this.Display.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtcon);
+            this.groupBox1.Controls.Add(this.txtpass);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(334, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 181);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose password";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.txtnumber);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.Type);
+            this.groupBox2.Location = new System.Drawing.Point(43, 69);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(255, 181);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Personal information";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnprocess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtnumber;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtcon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox Type;
+        private System.Windows.Forms.Button btnprocess;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.ListBox Display;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace mockexammmm2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int determinejoiningfee()
+        {
+
+            int joiningfee = 0;
+            string usertype = Type.SelectedItem.ToString();
+            switch (usertype)
+            {
+                case "Begginer":
+                    joiningfee = 120;
+                    break;
+
+                case "Intermediate":
+                    joiningfee = 120;
+                    break;
+
+                case "Advanced":
+                    joiningfee = 120;
+                    break;
+            }
+            return joiningfee;
+        }
+
+        private int Generateusername()
+        {
+            int username;
+            int luckynumber;
+            string firstname;
+            firstname = txtName.Text;
+            luckynumber = int.Parse(txtnumber.Text);
+
+          Random rand = new Random();
+            luckynumber = rand.Next(1,11);
+            username = int.Parse(firstname.ToString() + luckynumber);
+
+            return username;
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string usertype;
+            
+
+
+            StreamReader inputFile;
+
+            
+
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                inputFile = File.OpenText(openFileDialog1.FileName);
+                usertype = inputFile.ReadLine();
+               
+                
+                usertype = inputFile.ReadLine();
+                while (usertype != null)
+                {
+                    Type.Items.Add(usertype);
+                }
+
+                    
+
+             
+               
+            }
+
+            
+
+        }
+        
+
+        private void btnprocess_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            int lucky_number = int.Parse(txtnumber.Text);
+            string first_name = txtName.Text;
+            int Password = int.Parse(txtpass.Text);
+            int Confirm_password = int.Parse(txtcon.Text);
+
+
+
+
+            Generateusername();
+
+            determinejoiningfee();
+            Form1_Load(this, null);
+
+
+
+            Display.Items.Add("password is" + "\n" + Password + "\n" + "type of user is" +"\n" + "username is");
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtnumber.Text = "";
+            txtpass.Text = "";
+            txtcon.Text = "";
+            Display.Items.Clear();
+            Type.Items.Clear();
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
